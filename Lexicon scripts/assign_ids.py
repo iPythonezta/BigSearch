d61d4f5283b3a7ca8477f332fc6de4df03e4db09
iPythonezta <huzaifablogger.contact@gmail.com>
2025-11-24 16:20:29 +0500

    Ran the code for extracting words from json file, and then code for assigning ids and merging the 2 lexicons for html and json files. Also made modifications to the jsonParser.cpp to allow longer strings as they were representing sequence of DNAs

---
+++ Lexicon scripts/assign_ids.py
@@ -0,0 +1,14 @@
+import json
+def assign_ids_to_words(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        words = f.readlines()
+
+    id_map = {idx: word.strip() for idx, word in enumerate(words)}
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(id_map, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    assign_ids_to_words('final_words_lexicon.txt', 'lexicons_ids.json')
+
